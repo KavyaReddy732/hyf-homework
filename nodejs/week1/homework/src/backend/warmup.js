d19609e52175c2df8a387d72b49dd4ffaac7da5e
KavyaReddy732 <66561373+KavyaReddy732@users.noreply.github.com>
2020-10-29 23:46:47 +0100

    added nodejs/week1 homework with warmup exercise

---
+++ nodejs/week1/homework/src/backend/warmup.js
@@ -0,0 +1,26 @@
+class Circle {
+    constructor(radius){
+        this.radius = radius;
+    }
+
+    getDiameter() 
+    {
+        return 2*(this.radius);
+    } 
+    
+    getCircumference()
+    {
+        return 2 * Math.PI * this.radius;
+    }
+    
+    getArea()
+    {
+        return Math. PI * this. radius * this. radius;
+    }
+}
+
+
+const circle = new Circle(10);
+console.log(circle.getDiameter());
+console.log(circle.getCircumference());
+console.log(circle.getArea());
